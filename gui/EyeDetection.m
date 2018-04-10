8c282a24dcea5d1e9bfb5ccd68759bf77ca8675a
jim reilly <james.reilly@marquette.edu>
2018-04-10 13:32:00 -0500

    a MATLAB GUIDE generate gui for reading in an image file and over-plotting a circle

---
+++ gui/EyeDetection.m
@@ -0,0 +1,240 @@
+function varargout = EyeDetection(varargin)
+% EYEDETECTION MATLAB code for EyeDetection.fig
+%      EYEDETECTION, by itself, creates a new EYEDETECTION or raises the existing
+%      singleton*.
+%
+%      H = EYEDETECTION returns the handle to a new EYEDETECTION or the handle to
+%      the existing singleton*.
+%
+%      EYEDETECTION('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in EYEDETECTION.M with the given input arguments.
+%
+%      EYEDETECTION('Property','Value',...) creates a new EYEDETECTION or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before EyeDetection_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to EyeDetection_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help EyeDetection
+
+% Last Modified by GUIDE v2.5 10-Apr-2018 13:16:30
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @EyeDetection_OpeningFcn, ...
+                   'gui_OutputFcn',  @EyeDetection_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before EyeDetection is made visible.
+function EyeDetection_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to EyeDetection (see VARARGIN)
+
+% Choose default command line output for EyeDetection
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% This sets up the initial plot - only do when we are invisible
+% so window can get raised using EyeDetection.
+if strcmp(get(hObject,'Visible'),'off')
+    plot(rand(5));
+end
+
+set(handles.axes1,'visible','off') %hide the current axes
+set(get(handles.axes1,'children'),'visible','off') %hide the current axes contents
+
+% UIWAIT makes EyeDetection wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = EyeDetection_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in openInputFileDialog.
+function openInputFileDialog_Callback(hObject, eventdata, handles)
+% hObject    handle to openInputFileDialog (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[file path] = uigetfile('*.jpeg;*.jpg');
+filepath = strcat(path, file);
+set(handles.jpeg_file_path, 'String', filepath);
+
+
+% --------------------------------------------------------------------
+function FileMenu_Callback(hObject, eventdata, handles)
+% hObject    handle to FileMenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function OpenMenuItem_Callback(hObject, eventdata, handles)
+% hObject    handle to OpenMenuItem (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+file = uigetfile('*.fig');
+if ~isequal(file, 0)
+    open(file);
+end
+
+% --------------------------------------------------------------------
+function PrintMenuItem_Callback(hObject, eventdata, handles)
+% hObject    handle to PrintMenuItem (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+printdlg(handles.figure1)
+
+% --------------------------------------------------------------------
+function CloseMenuItem_Callback(hObject, eventdata, handles)
+% hObject    handle to CloseMenuItem (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+selection = questdlg(['Close ' get(handles.figure1,'Name') '?'],...
+                     ['Close ' get(handles.figure1,'Name') '...'],...
+                     'Yes','No','Yes');
+if strcmp(selection,'No')
+    return;
+end
+
+delete(handles.figure1)
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+     set(hObject,'BackgroundColor','white');
+end
+
+set(hObject, 'String', {'plot(rand(5))', 'plot(sin(1:0.01:25))', 'bar(1:.5:10)', 'plot(membrane)', 'surf(peaks)'});
+
+
+
+function jpeg_file_path_Callback(hObject, eventdata, handles)
+% hObject    handle to jpeg_file_path (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of jpeg_file_path as text
+%        str2double(get(hObject,'String')) returns contents of jpeg_file_path as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function jpeg_file_path_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to jpeg_file_path (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in openOutputFileDialog.
+function openOutputFileDialog_Callback(hObject, eventdata, handles)
+% hObject    handle to openOutputFileDialog (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[file path] = uigetfile('*.txt');
+filepath = strcat(path, file);
+set(handles.jpeg_file_path, 'String', filepath);
+
+
+function output_file_path_Callback(hObject, eventdata, handles)
+% hObject    handle to output_file_path (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of output_file_path as text
+%        str2double(get(hObject,'String')) returns contents of output_file_path as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function output_file_path_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to output_file_path (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in mainButton.
+function mainButton_Callback(hObject, eventdata, handles)
+% hObject    handle to mainButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+imagePath = get(handles.jpeg_file_path, 'String');
+if(isempty(imagePath))
+    fprintf('Error, enter input file first')
+else
+    inputImage = imread(imagePath);
+    image(inputImage);
+    set(handles.axes1,'visible','on') %hide the current axes
+    set(get(handles.axes1,'children'),'visible','on') %hide the current axes contents
+    hold on;
+    r = 40; x = 340; y = 175;
+    th = 0:pi/50:2*pi;
+    xunit = r * cos(th) + x;
+    yunit = r * sin(th) + y;
+    h = plot(xunit, yunit, 'r', 'LineWidth', 2);
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function axes1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: place code in OpeningFcn to populate axes1
